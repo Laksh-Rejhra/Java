0b677780cea78ef92de0e1db06b9cc04b20e8c03
Laksh-Rejhra <144317943+Laksh-Rejhra@users.noreply.github.com>
2024-01-21 23:47:58 -0800

    Add files via upload

---
+++ Behavior.java
@@ -0,0 +1,15 @@
+
+package behavior;
+
+public class Behavior{
+    
+    public static void main(String[] args){
+//        System.out.println("This is a long string that is the " + "Concaenation of two shorter string");
+//        System.out.println("The first computer was invented about " + 55 + " years ago.");
+//          System.out.println("8 plus 5 is " + 8 + 5); 
+//          System.out.println("8 plus 5 is " + (8 + 5)); 
+          System.out.println((8 + 5) + " 8 plus 5 is " ); 
+          
+          
+    }
+}
